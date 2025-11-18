1fa3c1bfa6836f55a7bce2384004f4db262feaf7
mmanjunath6232 <mmanjunath6232@gmail.com>
2025-11-18 17:20:26 +0530

    Add files via upload

---
+++ p10.c
@@ -0,0 +1,21 @@
+//10.print multiplication table
+
+#include <stdio.h>
+
+int main() {
+
+
+int n,i,mul=0;
+
+printf("Enter table: ");
+
+scanf("%d",&n);
+
+for(i=1;i<=10;i++)
+
+printf("%d*%d= %d\n",n,i,n*i);
+
+
+return 0;
+
+}
